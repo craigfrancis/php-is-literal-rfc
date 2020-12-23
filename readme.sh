359cf16faa0d752492b7b4ae7861fd4243db96f6
Craig Francis <craig@craigfrancis.co.uk>
2020-12-23 19:39:20 +0000

    Updated description and examples

--- readme.sh
+++ readme.sh
@@ -10,7 +10,7 @@ cat "readme.md" \
     -e 's/^    /  /g' \
     -e 's/\[([^]]+)\]\(([^\)]+)\)/\[\[\2|\1\]\]/g' \
   | perl -pe 's/\[\[([^\|]+)\|\1\]\]/\1/g' \
-  | sed -E -e '1h;2,$H;$!d;g' -e 's/(\nhttp[^ ]+)(\nhttp)/\1\\\\\2/g' \
+  | sed -E -e '1h;2,$H;$!d;g' -e 's/(\nhttp[^ ]+)  (\nhttp)/\1\\\\\2/g' \
   > readme.txt;
 
 # cat "readme.txt" \
