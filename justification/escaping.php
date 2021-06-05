f74fbb877c59f1cd7fb8b09967d8f27ddd391e60
Craig Francis <craig@craigfrancis.co.uk>
2021-06-05 17:36:19 +0100

    Add initial justification scripts

---
+++ justification/escaping.php
@@ -0,0 +1,123 @@
+<?php
+
+//--------------------------------------------------
+// No Escaping
+
+	$_GET['id'] = 'id';
+
+	$sql = 'WHERE id = ' . $_GET['id'];
+
+//--------------------------------------------------
+// Bad Escaping
+
+	$_GET['id'] = 'id';
+
+	$sql = 'WHERE id = ' . $mysqli->real_escape_string($_GET['id']);
+
+//--------------------------------------------------
+// Bad Escaping
+
+	// Intended HTML usage:
+	//   <input type="checkbox" name="delete[]" value="1" />
+	//   <input type="checkbox" name="delete[]" value="3" />
+
+	$_POST['delete'] = ['1) OR (1 = 1'];
+
+	$sql = 'DELETE FROM table WHERE id IN (' . implode(',', $_POST['delete']) . ')'; // INSECURE
+
+		// While array_map('intval') might fix this instance today, who knows who will copy/edit it in the future.
+		// Instead, just use parameterised queries:
+		//
+		// Levi Morrison             = https://stackoverflow.com/a/23641033/538216
+		// PDO Execute               = https://www.php.net/manual/en/pdostatement.execute.php#example-1012
+		// Drupal Multiple Arguments = https://www.drupal.org/docs/7/security/writing-secure-code/database-access#s-multiple-arguments
+
+//--------------------------------------------------
+// Possible config issue
+// If 'sql_mode' includes NO_BACKSLASH_ESCAPES
+
+	$_GET['id'] = '2" or "1"="1';
+
+	$sql = 'SELECT 1 FROM user WHERE id = "' . $mysqli->escape_string($_GET['id']) . '"';
+
+//--------------------------------------------------
+// Possible config issue
+// If 'SET NAMES latin1' has been used, and escape_string() continues to use 'utf8'.
+
+	$_GET['name'] = 'Zoë';
+
+	$sql = 'INSERT INTO user (name) VALUES ("' . $mysqli->escape_string($_GET['name']) . '")';
+
+//--------------------------------------------------
+// No Escaping
+
+	$_GET['url'] = '/ onerror=alert(1)';
+
+	$html = '<img src=' . $_GET['url'] . ' alt="" />';
+
+//--------------------------------------------------
+// Missing Quotes
+
+	$_GET['url'] = '/ onerror=alert(1)';
+
+	$html = "<img src=" . htmlentities($_GET['url']) . " alt='' />";
+
+//--------------------------------------------------
+// Bad Escaping
+// htmlentities() doesn't encode single quotes by default.
+// I got this fixed for PHP 8.1
+// https://github.com/php/php-src/commit/50eca61f68815005f3b0f808578cc1ce3b4297f0
+
+	$_GET['url'] = "/' onerror='alert(1)";
+
+	$html = "<img src='" . htmlentities($_GET['url']) . "' alt='' />";
+
+//--------------------------------------------------
+// Context Issue
+
+	$_GET['url'] = 'javascript:alert(1);';
+
+	$html = '<a href="' . htmlentities($_GET['url']) . '">Link</a>';
+
+//--------------------------------------------------
+// Context Issue
+// The browsers HTML parser is not aware of escaped JavaScript strings.
+
+	$_GET['url'] = '</script><script>alert(1)</script>';
+
+	$html = '<script> var url = "' . addslashes($_GET['url']) . '"; </script>';
+
+//--------------------------------------------------
+// Incomplete Escaping (ref urlencode)
+
+	$_GET['name'] = 'A&B';
+
+	$html = '<a href="./?name=' . htmlentities($_GET['name']) . '">Link</a>';
+
+//--------------------------------------------------
+// Encoding
+// PHP just assumes the string is UTF-8 (since 5.4).
+// Without a charset declaration, the browser will also guess at the encoding.
+// For example, this classic UTF-7 value for Internet Explorer.
+
+	$_GET['value'] = '+ADw-script+AD4-alert(1)+ADw-+AC8-script+AD4-';
+
+	header('Content-Type: text/html; charset=');
+	header('X-Content-Type-Options: -');
+
+	$html = '<p>' . htmlentities($_GET['value']) . '</p>';
+
+//--------------------------------------------------
+// General weirdness
+
+	$_GET['email'] = 'b@example.com -X/www/example.php';
+
+	$parameters = '-f' . $_GET['email'];
+
+	// $parameters = '-f' . escapeshellarg($_GET['email']);
+
+	mail('a@example.com', 'Subject', 'Message', NULL, $parameters);
+
+	// It's not possible to safely escape values in $additional_parameters for mail()
+
+?>
---
+++ justification/mistakes.php
@@ -0,0 +1,113 @@
+<?php
+
+//--------------------------------------------------
+// Eloquent, DB::select
+// https://laravel.com/docs/8.x/database#running-a-select-query
+// Can only assume the programmer has written the SQL, and provided the user values separately.
+
+	$_GET['active'] = 'active';
+
+	$users = DB::select('select * from users where active = ' . $_GET['active']); // INSECURE
+
+	$users = DB::select('select * from users where active = ?', [$_GET['active']]);
+
+//--------------------------------------------------
+// Doctrine, Query Builder
+// https://www.doctrine-project.org/projects/doctrine-orm/en/current/reference/query-builder.html#high-level-api-methods
+
+	$_GET['id'] = 'id';
+
+	$qb->select('u')
+	   ->from('User', 'u')
+	   ->where('u.id = ' . $_GET['id']); // INSECURE
+
+	$qb->select('u')
+	   ->from('User', 'u')
+	   ->where('u.id = :identifier')
+	   ->setParameter('identifier', $_GET['id']);
+
+//--------------------------------------------------
+// Doctrine, DQL
+// https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/reference/dql-doctrine-query-language.html
+// An abstraction from SQL, with the same issues
+
+	$_GET['id'] = 'id';
+
+	$query = $em->createQuery('SELECT u FROM User u WHERE u.id = ' . $_GET['id']); // INSECURE
+
+	$query = $em->createQuery('SELECT u FROM User u WHERE u.id = ?1')->setParameter(1, $_GET['id']);
+
+//--------------------------------------------------
+// CakePHP, Query Builder
+// https://book.cakephp.org/3/en/orm/query-builder.html
+// Parts of the where() array need to be literals, because those string are added to the SQL:
+
+	$_GET['id'] = 'id';
+
+	$query = $articles->find()->where(['id != ' . $_GET['id']]); // INSECURE
+
+	$query = $articles->find()->where(['id !=' => $_GET['id']]);
+
+//--------------------------------------------------
+// CakePHP, Retrieving Data
+// https://book.cakephp.org/3/en/orm/retrieving-data-and-resultsets.html
+// https://book.cakephp.org/4/en/orm/query-builder.html#advanced-conditions
+// Similar to the last one, but seeing these on production sites is, worrying:
+
+	$_GET['category'] = 'category_id';
+
+	$query = $articles->find('all', [
+		'conditions' => [
+			'OR' => [
+				'category_id IS NULL',
+				'category_id = ' . $_GET['category'], // INSECURE
+			],
+		],
+	]);
+
+//--------------------------------------------------
+// CakePHP, Connection Class
+// https://book.cakephp.org/3/en/orm/database-basics.html#connection-classes
+// The $sql argument, in methods like `query()` shouldn't allow non-literal values.
+// https://github.com/cakephp/cakephp/blob/master/src/Database/Connection.php#L353
+
+	$_GET['id'] = 'id';
+
+	$statement = $connection->query('UPDATE articles SET published = 1 WHERE id = ' . $_GET['id']); // INSECURE
+
+	$statement = $connection->query('UPDATE articles SET published = 1 WHERE id = ?', [$_GET['id']]);
+
+//--------------------------------------------------
+// RedBean, find
+// https://redbeanphp.com/index.php?p=/finding
+// "Never put user input directly in your query!"
+// The `$sql` argument needs to be a literal.
+// https://github.com/gabordemooij/redbean/blob/master/RedBeanPHP/Finder.php#L191
+
+	$_GET['id'] = 'id';
+
+	$users = R::find('user', 'id = ' . $_GET['id']); // INSECURE
+
+	$users = R::find('user', 'id = ? ', [$_GET['id']]);
+
+//--------------------------------------------------
+// Propel, Model Criteria
+// http://propelorm.org/Propel/reference/model-criteria.html
+
+	$_GET['id'] = 'id';
+
+	$users = UserQuery::create()->where('id = ' . $_GET['id'])->find(); // INSECURE
+
+	$users = UserQuery::create()->where('id = ?', $_GET['id'])->find();
+
+//--------------------------------------------------
+// Twig, createTemplate
+// https://twig.symfony.com/doc/2.x/recipes.html#loading-a-template-from-a-string
+
+	$_GET['name'] = '<script>alert(1)</script>';
+
+	$html = $twig->createTemplate('<p>Hi ' . $_GET['name'] . '</p>')->render(); // INSECURE
+
+	$html = $twig->createTemplate('<p>Hi {{ name }}</p>')->render(['name' => $_GET['name']]);
+
+?>
