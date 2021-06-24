b8aa93d2702c179c0ebd473809803f24843d9633
Craig Francis <craig@craigfrancis.co.uk>
2021-06-24 16:05:00 +0100

    Update test run for A and F

--- tests/run.sh
+++ tests/run.sh
@@ -26,25 +26,25 @@ export SCRIPT_FILENAME="./600-symfony${SCRIPT_NAME}";
 
 rm -f output.txt;
 
-echo "Test 1" | tee -a output.txt;
+echo "Test 1 / 001.phpt" | tee -a output.txt;
 
-for V in $(printf 'a b c %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cli/php ./001.phpt >> output.txt; done
+for V in $(printf 'a f %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cli/php ./001.phpt >> output.txt; done
 
-echo "Test 2" | tee -a output.txt;
+echo "Test 2 / Laravel" | tee -a output.txt;
 
 export SCRIPT_FILENAME="./500-laravel${SCRIPT_NAME}";
 export APP_ENV="production"
 export APP_DEBUG=false
 
-for V in $(printf 'a b c %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cgi/php-cgi "-T10" > /dev/null 2>> output.txt; done
+for V in $(printf 'a f %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cgi/php-cgi "-T10" > /dev/null 2>> output.txt; done
 
-echo "Test 3" | tee -a output.txt;
+echo "Test 3 / Symfony" | tee -a output.txt;
 
 export SCRIPT_FILENAME="./600-symfony${SCRIPT_NAME}";
 export APP_ENV="prod"
 
-for V in $(printf 'a b c %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cgi/php-cgi "-T10" > /dev/null 2>> output.txt; done
+for V in $(printf 'a f %.0s' {1..11}); do echo -n $V >> output.txt; ../src/$V/sapi/cgi/php-cgi "-T10" > /dev/null 2>> output.txt; done
 
-perl -0777 -pe 's/(^|\n)a( |\nElapsed time: )([0-9\.]+)( sec)?\nb( |\nElapsed time: )([0-9\.]+)( sec)?\nc( |\nElapsed time: )([0-9\.]+)( sec)?/\n$3\t$6\t$9/g' -i output.txt;
+perl -0777 -pe 's/(^|\n)a( |\nElapsed time: )([0-9\.]+)( sec)?\nf( |\nElapsed time: )([0-9\.]+)( sec)?/\n$3\t$6/g' -i output.txt;
 
 echo "Done";
