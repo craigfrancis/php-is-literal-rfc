136ea84d28c96a70f69f0c368a5b4f267ab5a3fe
Craig Francis <craig@craigfrancis.co.uk>
2021-12-06 15:46:43 +0000

    Update Apache Maven version

--- others/java/index.sh
+++ others/java/index.sh
@@ -1,9 +1,9 @@
 
 if [[ ! -d './maven/' ]]; then
-	curl 'https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.zip' --output 'apache-maven-3.8.3-bin.zip'
-	unzip './apache-maven-3.8.3-bin.zip';
-	mv './apache-maven-3.8.3' './maven'
-	rm -rf 'apache-maven-3.8.3-bin.zip';
+	curl 'https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.zip' --output 'apache-maven-3.8.4-bin.zip'
+	unzip './apache-maven-3.8.4-bin.zip';
+	mv './apache-maven-3.8.4' './maven'
+	rm -rf 'apache-maven-3.8.4-bin.zip';
 fi
 
 ./maven/bin/mvn package
