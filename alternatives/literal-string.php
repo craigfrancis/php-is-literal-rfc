61265d0f1cab22ab60ff533dc3af606c88e8c532
Craig Francis <craig@craigfrancis.co.uk>
2023-03-10 17:15:51 +0000

    Start adding alternative examples

---
+++ alternatives/literal-string.php
@@ -0,0 +1,80 @@
+<?php
+
+//--------------------------------------------------
+// Doctrine QueryBuilder
+
+	$qb->select('u')
+		->from('User', 'u')
+		->where('u.id = ?1')
+		->setParameter(1, $_GET['id']);
+
+	$qb->select('u')
+		->from('User', 'u')
+		->where($qb->expr()->andX(
+			$qb->expr()->eq('u.type_id', '?1'),
+			$qb->expr()->isNull('u.deleted'),
+		))
+		->setParameter(1, $_GET['type_id']);
+
+//--------------------------------------------------
+// Laravel
+
+	DB::table('user')->whereRaw('CONCAT(name_first, " ", name_last) LIKE ?', $search . '%');
+
+//--------------------------------------------------
+// Plain SQL
+
+	//--------------------------------------------------
+
+		$parameters = [];
+		$identifiers = [];
+		$where_sql = 'u.deleted IS NULL';
+
+	//--------------------------------------------------
+
+		$name = ($_GET['name'] ?? '');
+		if ($name) {
+
+			$where_sql .= ' AND
+				u.name LIKE ?';
+
+			$parameters[] = '%' . $name . '%';
+
+		}
+
+	//--------------------------------------------------
+
+		$sql = '
+			SELECT
+				u.name,
+				u.email
+			FROM
+				user AS u
+			WHERE
+				' . $where_sql;
+
+	//--------------------------------------------------
+
+		$order = ($_GET['sort'] ?? 'email');
+
+		$sql .= '
+			ORDER BY
+				{o}';
+
+		$identifiers['o'] = $order;
+
+	//--------------------------------------------------
+
+		$sql .= '
+			LIMIT
+				?, 10';
+
+		$parameters[] = intval($_GET['page'] ?? 0);
+
+	//--------------------------------------------------
+
+		print_r([$sql, $parameters, $identifiers]);
+
+		// $db->query($sql, $parameters, $identifiers);
+
+?>
